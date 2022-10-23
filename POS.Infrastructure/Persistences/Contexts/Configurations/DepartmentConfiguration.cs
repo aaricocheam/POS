d093cb0e9d6ae9941a343a5ebcbf1894b708139d
Anthony Aricochea <aaricoch@everis.com>
2022-10-23 01:16:32 -0500

    Configure Entities and DBContext

---
+++ POS.Infrastructure/Persistences/Contexts/Configurations/DepartmentConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using POS.Domain.Entities;
+
+namespace POS.Infrastructure.Persistences.Contexts.Configurations
+{
+    public class DepartmentConfiguration : IEntityTypeConfiguration<District>
+    {
+        public void Configure(EntityTypeBuilder<District> builder)
+        {
+            builder.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+        }
+    }
+}
