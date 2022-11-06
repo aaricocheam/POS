827f66d1d130af4da3015cb0281d55ce30ca2601
Anthony Aricochea <aaricoch@everis.com>
2022-11-06 01:22:52 -0500

    application layer, automapper, fluent validator

---
+++ POS.Application/Interfaces/ICategoryApplication.cs
@@ -0,0 +1,18 @@
+﻿using POS.Application.Commons.Bases;
+using POS.Application.Dtos.Request;
+using POS.Application.Dtos.Response;
+using POS.Infrastructure.Commons.Bases.Request;
+using POS.Infrastructure.Commons.Bases.Response;
+
+namespace POS.Application.Interfaces
+{
+    public interface ICategoryApplication
+    {
+        Task<BaseResponse<BaseEntityResponse<CategoryResponseDto>>> ListCategories(BaseFiltersRequest filters);
+        Task<BaseResponse<IEnumerable<CategorySelectResponseDto>>> ListSelectCategories();
+        Task<BaseResponse<CategoryResponseDto>> CategoryById(int categoryId);
+        Task<BaseResponse<bool>> RegisterCategoty(CategoryRequestDto requestDto);
+        Task<BaseResponse<bool>> EditCategoty(int categoryId, CategoryRequestDto requestDto);
+        Task<BaseResponse<bool>> RemoveCategoty(int categoryId);
+    }
+}
---
+++ POS.Application/Mappers/CategoryMappingsProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using POS.Application.Dtos.Request;
+using POS.Application.Dtos.Response;
+using POS.Domain.Entities;
+using POS.Infrastructure.Commons.Bases.Response;
+using POS.Utilities.Static;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Application.Mappers
+{
+    public class CategoryMappingsProfile : Profile
+    {
+        protected CategoryMappingsProfile()
+        {
+            CreateMap<Category, CategoryResponseDto>()
+                .ForMember(x => x.CategoryId, x => x.MapFrom(y => y.Id))
+                .ForMember(x => x.StateCategory, x => x.MapFrom(y => y.State.Equals((int)StateType.Active) ? "Activo" : "Inactivo"))
+                .ReverseMap();
+            CreateMap<BaseEntityResponse<Category>, BaseEntityResponse<CategoryResponseDto>>()
+                .ReverseMap();
+            CreateMap<CategoryRequestDto, Category>();
+            CreateMap<Category, CategorySelectResponseDto>()
+                .ForMember(x => x.CategoryId, x => x.MapFrom(y => y.Id))
+                .ReverseMap();
+        }
+    }
+}
