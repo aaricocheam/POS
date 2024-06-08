e9e1398f7bd6857eba7299fb7a5d2d8559c341c4
anthony <anthony>
2024-06-08 14:43:58 -0500

    Mejora del patron repository - GenericRepository

--- POS.Application/Mappers/CategoryMappingProfile.cs
+++ POS.Application/Mappers/CategoryMappingProfile.cs
@@ -12,12 +12,14 @@ namespace POS.Application.Mappers
         public CategoryMappingProfile()
         {
             CreateMap<Category, CategoryResponseDto>()
+                .ForMember(x => x.CategoryId, x => x.MapFrom(y => y.Id))
                 .ForMember(x => x.StateCategory, x => x.MapFrom(y => y.State.Equals((int)StateTypes.Active) ? "Activo" : "Inactivo"))
                 .ReverseMap();
             CreateMap<BaseEntityResponse<Category>, BaseEntityResponse<CategoryResponseDto>>()
                 .ReverseMap();
             CreateMap<CategoryRequestDto, Category>();
             CreateMap<Category, CategorySelectResponseDto>()
+                .ForMember(x => x.CategoryId, x => x.MapFrom(y => y.Id))
                 .ReverseMap();
         }
     }
--- POS.Infrastructure/Persistences/Interfaces/IGenericRepository.cs
+++ POS.Infrastructure/Persistences/Interfaces/IGenericRepository.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using POS.Domain.Entities;
+using POS.Infrastructure.Commons.Bases.request;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace POS.Infrastructure.Persistences.Interfaces
 {
-    public interface IGenericRepository<T> where T : class
+    public interface IGenericRepository<T> where T : BaseEntity
     {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<bool> RegisterAsync(T entity);
+        Task<bool> EditAsync(T entity);
+        Task<bool> RemoveAsync(int id);
+        IQueryable<T> GetEntityQuery(Expression<Func<T, bool>>? filter = null);
+        IQueryable<TDTO> Ordering<TDTO>(BasePaginationRequest request, IQueryable<TDTO> queryable, bool pagination = false) where TDTO : class;
     }
 }
