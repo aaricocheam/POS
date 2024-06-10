caf0bb0a7abbd2323c3f3533d0e1a8bb477ad4ad
anthony <anthony>
2024-06-10 12:20:33 -0500

    agregando cors

--- POS.Api/Program.cs
+++ POS.Api/Program.cs
@@ -4,6 +4,8 @@ using POS.Infrastructure.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 var Configuration = builder.Configuration;
 // Add services to the container.
+var Cors = "Cors";
+
 builder.Services.AddInjectionInfrastructure(Configuration);
 builder.Services.AddInjectionApplication(Configuration);
 
@@ -12,7 +14,20 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: Cors,
+        builder =>
+        {
+            builder.WithOrigins("*");
+            builder.AllowAnyMethod();
+            builder.AllowAnyHeader();
+        });
+});
+
 var app = builder.Build();
+
+app.UseCors(Cors);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
